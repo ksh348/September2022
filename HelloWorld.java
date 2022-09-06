3368a8aa2174134c7b34b34d6a7b7f1876293485
Sridhar Kokkula <107347714+ksh348@users.noreply.github.com>
2022-09-06 14:28:41 +0530

    Add files via upload
    First Change

---
+++ HelloWorld.java
@@ -0,0 +1,47 @@
+/*
+Java Micro Device Applications
+
+Java Web based Applications
+
+Java Desktop based Applications --- main
+
+package 
+
+java.lang.System
+*/
+
+public class HelloWorld {
+
+	public static void main(String argc[]){
+		System.out.print("Hello "); //ln
+		System.out.print("World");
+	}
+
+}
+
+
+/*
+int main(){
+ printf(); // cout << 
+}
+
+char main(){
+
+}
+
+void main(){
+
+}
+
+int main(char *argv[], int argc){
+
+}
+
+char main(char *argv[], int argc){
+
+}
+
+void main(char *argv[], int argc){
+
+}
+*/
